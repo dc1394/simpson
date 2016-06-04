2ae4f27c04efd44ab4d50c9d43d7c4c93bf2148d
dc1394 <dc1394@hotmail.com>
2016-06-04 11:03:47 +0900

    取りあえず追加

---
+++ simpson/simpson.h
@@ -0,0 +1,116 @@
+﻿/*! \file simpson.h
+    \brief simpsonの公式で数値積分を行うクラスの宣言
+
+    Copyright ©  2016 @dc1394 All Rights Reserved.
+*/
+#ifndef _SIMPSON_H_
+#define _SIMPSON_H_
+
+#pragma once
+
+#include "functional.h"
+#include <array>                            // for std::array
+#include <cstdint>                          // for std::int32_t
+#include <vector>                           // for std::vector
+#include <intrin.h>                         // for _xgetbv
+#include <boost/align/aligned_allocator.hpp>// for boost::alignment::aligned_allocator
+#include <boost/mpl/int.hpp>
+#include <dvec.h>                           // for F64vec4, F64vec2
+
+namespace simpson {
+    //! A class.
+    /*!
+        simpsonの公式で数値積分を行うクラス
+    */
+	class Simpson final
+	{
+    public:
+        // #region コンストラクタ
+
+        //! A constructor.
+        /*!
+            唯一のコンストラクタ
+            \param n simpsonの公式の分点
+        */
+        Simpson(std::int32_t n);
+
+        // #endregion コンストラクタ
+
+        // #region メンバ関数
+
+        template <typename FUNCTYPE, int V>
+        double simpson(myfunctional::Functional<FUNCTYPE> const & func, double x1, double x2) {
+            return operator()(func, x1, x2, boost::mpl::int_<V>());
+        }
+
+    private:
+        //! A private member function (template function).
+        /*!
+            Simpsonの公式によって数値積分を実行する
+            \param func 被積分関数
+            \param x1 積分の下端
+            \param x2 積分の上端
+            \return 積分値
+        */
+        template <typename FUNCTYPE>
+        double operator()(myfunctional::Functional<FUNCTYPE> const & func, double x1, double x2, boost::mpl::int_<0>) const;
+
+        //! A private member function.
+        /*!
+            AVX命令が使用可能かどうかをチェックする
+            \return AVX命令が使用可能ならtrue、使用不可能ならfalse
+        */
+        bool availableAVX() const;
+
+        // #endregion メンバ関数
+
+        // #region メンバ変数
+
+        //! A private member variable (constant).
+        /*!
+            AVX命令が使用可能かどうか
+        */
+        bool const avxSupported;
+
+        //! A private member variable (constant).
+        /*!
+            simpsonの公式の分点
+        */
+        std::uint32_t const n_;
+
+        // #endregion メンバ変数
+
+        // #region 禁止されたコンストラクタ・メンバ関数
+
+        //! A private constructor (deleted).
+        /*!
+            デフォルトコンストラクタ（禁止）
+        */
+        Simpson() = delete;
+
+        //! A private copy constructor (deleted).
+        /*!
+            コピーコンストラクタ（禁止）
+        */
+		Simpson(Simpson const &) = delete;
+
+        //! A private member function (deleted).
+        /*!
+            operator=()の宣言（禁止）
+            \param コピー元のオブジェクト
+            \return コピー元のオブジェクト
+        */
+		Simpson & operator=(Simpson const &) = delete;
+
+        // #endregion 禁止されたコンストラクタ・メンバ関数
+	};
+    
+    template <typename FUNCTYPE>
+    inline double Simpson::operator()(myfunctional::Functional<FUNCTYPE> const & func, double x1, double x2, boost::mpl::int_<0>) const
+    {
+        return 0.0;
+    }
+}
+
+#endif  // _SIMPSON_H_
+
---
+++ simpson/simpson_main.cpp
@@ -0,0 +1,27 @@
+﻿//#include "checkpoint.h"
+#include "simpson.h"
+#include <array>            // for std::array
+#include <cmath>            // for std::sqrt
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+namespace {
+    static auto constexpr DIGIT = 15U;
+    static auto constexpr LOOPMAX = 1000000000UL;
+    static auto constexpr N = 10001U;
+}
+
+int main()
+{
+    auto const func = myfunctional::make_functional([](double x) { return 1.0 / (2.0 * std::sqrt(x)); });
+	
+    simpson::Simpson s(100);
+    s.simpson<decltype(func), 0>(func, 1.0, 4.0);
+
+    //std::cout << "正確な値：\t" << std::setprecision(DIGIT) << exact  << '\n';
+	//std::cout << "AVX無効：\t" << std::setprecision(DIGIT) << res[0] << '\n';
+    //std::cout << "AVX有効：\t" << std::setprecision(DIGIT) << res[1] << '\n';
+
+	return 0;
+}
